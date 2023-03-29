5b6b0a5614ba7b9b9fe1b74c7abeb0519464df06
plazmica10 <ognjenstancevic@gmail.com>
2023-03-29 20:41:55 +0200

    initial commit

---
+++ src/main/java/tim2/cataloging/tim2/Tim2Application.java
@@ -0,0 +1,13 @@
+package tim2.cataloging.tim2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tim2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tim2Application.class, args);
+	}
+
+}
