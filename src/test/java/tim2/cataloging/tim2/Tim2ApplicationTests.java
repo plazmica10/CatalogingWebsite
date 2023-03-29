9ee136536ffb67197a37cffb824214409a6cb995
plazmica10 <ognjenstancevic@gmail.com>
2023-03-29 20:41:55 +0200

    initial commit

---
+++ src/test/java/tim2/cataloging/tim2/Tim2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tim2.cataloging.tim2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tim2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
