a40a2095c634f00221bb3b4d28bfbbebfff048e1
plazmica10 <ognjenstancevic@gmail.com>
2023-05-16 23:31:37 +0200

    Review,Genre - CRUD

--- src/main/java/tim2/cataloging/tim2/model/Genre.java
+++ src/main/java/tim2/cataloging/tim2/model/Genre.java
@@ -6,8 +6,32 @@ import java.io.Serializable;
 public class Genre implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(unique = true)
     private String name;
+
+    public Genre() {
+    }
+
+    public Genre(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
