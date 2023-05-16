cb1a969a377e452cb1e04d1ae22a16a0adee42b4
NovakovicIvan <novakovic.ivan002@gmail.com>
2023-05-17 00:08:50 +0200

    User - CRUD

--- src/main/java/tim2/cataloging/tim2/model/User.java
+++ src/main/java/tim2/cataloging/tim2/model/User.java
@@ -43,4 +43,108 @@ public class User implements Serializable {
     @OneToMany
     private List<Shelf> shelves;
 
+    // CONSTRUCTORS
+    public User(String username, String name, String surname, String email, String password, Date date, String photo, String description, ROLE role) {
+        this.username = username;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.date = date;
+        this.photo = photo;
+        this.description = description;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    // GETTERS AND SETTERS
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(ROLE role) {
+        this.role = role;
+    }
+
+    public List<Shelf> getShelves() {
+        return shelves;
+    }
+
+    public void setShelves(List<Shelf> shelves) {
+        this.shelves = shelves;
+    }
 }
