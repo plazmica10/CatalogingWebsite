f3df496abef0093003842c4d9519d134f6ed12ce
NovakovicIvan <novakovic.ivan002@gmail.com>
2023-05-16 22:11:41 +0200

    Book - CRUD

--- src/main/java/tim2/cataloging/tim2/configuration/DatabaseConfiguration.java
+++ src/main/java/tim2/cataloging/tim2/configuration/DatabaseConfiguration.java
@@ -3,7 +3,11 @@ package tim2.cataloging.tim2.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import tim2.cataloging.tim2.repository.BookRepository;
 
 @Configuration
 public class DatabaseConfiguration {
+
+    @Autowired
+    private BookRepository bookRepository;
 }
--- src/main/java/tim2/cataloging/tim2/model/Book.java
+++ src/main/java/tim2/cataloging/tim2/model/Book.java
@@ -34,8 +34,8 @@ public class Book implements Serializable{
     @ManyToOne
     private Genre genre;
 
-    public Book(Long id, String ISBN, String title, String photo, Date date, int pages, String description, int rating, Genre genre) {
-        this.id = id;
+    // CONSTRUCTORS
+    public Book(String ISBN, String title, String photo, Date date, int pages, String description, int rating) {
         this.ISBN = ISBN;
         this.title = title;
         this.photo = photo;
@@ -43,6 +43,81 @@ public class Book implements Serializable{
         this.pages = pages;
         this.description = description;
         this.rating = rating;
+    }
+
+    public Book() {
+    }
+
+    // GETTERS AND SETTERS
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
         this.genre = genre;
     }
 }
