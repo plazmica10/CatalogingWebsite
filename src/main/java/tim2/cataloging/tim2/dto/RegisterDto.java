e03bfb765f66e22f8533496c3c29c3a16e458ea0
plazmica10 <ognjenstancevic@gmail.com>
2023-05-20 02:27:09 +0200

    register user.v2

---
+++ src/main/java/tim2/cataloging/tim2/dto/RegisterDto.java
@@ -0,0 +1,65 @@
+package tim2.cataloging.tim2.dto;
+
+import tim2.cataloging.tim2.model.User;
+
+public class RegisterDto {
+    private String name;
+
+    private String surname;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private RegisterDto(){
+    }
+    private RegisterDto(String name, String surname, String username, String email, String password){
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
