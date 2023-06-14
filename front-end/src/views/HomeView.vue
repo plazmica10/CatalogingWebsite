4aa6a6cfaed1d2fe5e49757c2ac9e4d1aca2cecb
plazmica10 <ognjenstancevic@gmail.com>
2023-06-14 18:07:39 +0200

    bg

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Welcome</h1>
+  <div class="home-view">
+  <p class="paragraf">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores natus, ad libero expedita adipisci, voluptates exercitationem voluptas, ipsam nesciunt odio praesentium molestiae recusandae cum? Delectus similique accusantium eius magni amet?
+  Et eius sit fugiat ullam itaque vitae quaerat totam explicabo dolores animi laboriosam quam iusto voluptas aperiam dolorem corporis eligendi, perspiciatis minus minima cupiditate rem maxime laborum? Iusto, molestias reprehenderit.
+  Odio, asperiores dignissimos. Labore fugit laboriosam alias temporibus molestias. Excepturi, illo laboriosam mollitia quasi quidem quis nesciunt! Culpa maiores error fuga porro, esse earum ipsa tempore sapiente, commodi velit minus.
+  </p>
+</div>
 </template>
 
 <script>
@@ -7,3 +12,23 @@ export default {
   name: "HomeView",
 };
 </script>
+
+<style>
+
+.home-view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  height: 95vh;
+  background-image:linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),url("../assets/cover.jpg");
+  border: 1px solid red;
+  background-size: cover;
+}
+
+.home-view .paragraf{
+  width: 50%;
+  font-family: "Montserrat"; 
+}
+</style>
