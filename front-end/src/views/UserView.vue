86a089976d8f779b03aeb84482a1bfcc7ca0a8a6
plazmica10 <ognjenstancevic@gmail.com>
2023-06-22 14:39:46 +0200

    Sending,accepting and rejecting requests
    UserView parcijalno radi, ne prikazuje shelfove atm

--- front-end/src/views/UserView.vue
+++ front-end/src/views/UserView.vue
@@ -3,7 +3,18 @@
         <h1 v-if="user">{{ user.name }}</h1>
         <p v-if="user">Email: {{ user.email }}</p>
         <p v-if="user">Role: {{ user.role }}</p>
-        <p v-else>No user found.</p>
+        <div v-if="user.role == 'AUTHOR'">
+          <button class="btn" style="background-color: #252422; color: white;text-decoration:double;" @click="showDialog = true; console.log(showDialog)">Send request</button>
+          <dialog v-if="showDialog" role="dialog" aria-modal="true" open>
+            <form @submit.prevent="submitForm">
+              <input type="email" v-model="email" placeholder="Email" required>
+              <input type="phone" v-model="phone" placeholder="Phone">
+              <input type="text" v-model="message" placeholder="Message">
+              <button type="submit">Send</button>
+            </form>
+          </dialog>
+        </div>
+        <p v-if="!user">No user found.</p>
     </div>
 <div class="container" style="border: 1px solid red;">
     <div v-for="shelf in shelves" :key="shelf.id">
@@ -29,6 +40,10 @@ export default {
   name: "UserView",
 
   data: () => ({
+    showDialog: false,
+    email: "",
+    phone: "",
+    message: "",
     inShelf: {
       id: 0,
       isPrimary: false,
@@ -36,12 +51,19 @@ export default {
       shelfItems: []
     },
     shelves: [],
-    user: null,
+    user:{
+      username: "",
+      name: "",
+      surname: "",
+      email: "",
+      description: "",
+      role: "",
+      shelves: []
+    }
   }),
   mounted: function() {
-    //TRENUTNO NE RADI JER VADI SAMO IZ LOGOVANOG ID TAKO DA RADI SAMO ONAJ 
-    //KAD LOGOVAN KLIKNE NA ONU IKONICU U NAV
-    const userId = this.$store.state.user.id;
+
+    const userId = this.$route.params.id;
     axios
         .get("http://localhost:9090/users/" + userId, {withCredentials: true})
         .then((res) => {
@@ -55,14 +77,14 @@ export default {
          .get("http://localhost:9090/shelves", {withCredentials: true})
          .then((res) => {
             this.shelves = res.data;
-            // console.log(this.shelves);
+            console.log(this.shelves);
 
             this.shelves.forEach(shelf => {
     axios
                 .get("http://localhost:9090/shelves/" + shelf.id, {withCredentials: true})
                 .then((res) => {
                     shelf.shelfItems = res.data.shelfItems;
-                    // console.log(shelf.shelfItems);
+                    console.log(shelf.shelfItems);
                 })
                 .catch((error) => {
                     console.log(error);
@@ -72,7 +94,24 @@ export default {
          .catch((error) => {
            console.log(error);
          });
-  }
+  },
+  methods: {
+    submitForm() {
+      axios
+        .post(`http://localhost:9090/requests/${this.user.id}`, {
+          email: this.email,
+          phone: this.phone,
+          message: this.message,
+        }, { withCredentials: true })
+        .then((res) => {
+          window.alert("Request sent!");
+          this.showDialog = false;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
