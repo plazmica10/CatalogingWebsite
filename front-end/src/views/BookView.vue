8df3ae4b33d33b99b1a5532b998f8827c9780955
NovakovicIvan <novakovic.ivan002@gmail.com>
2023-06-22 21:58:12 +0200

    Prelazim na komp mraleaeae
    Nista ne radi lmao

--- front-end/src/views/BookView.vue
+++ front-end/src/views/BookView.vue
@@ -5,9 +5,21 @@
             <div class="d-flex justify-content-center">
                 <div class="book-container">
                     <div class="book">
-                        <!-- <img :src="require('@/assets/' + book.photo)"> -->
+                        <!-- <img :src="require('@/assets/' + this.book.photo)"> -->
+                        <!-- <img v-if="isReady" :src="require(this.photoLink)"> -->
+                        <img :src="photoLink" alt="">
+
+                        <p>{{ photoLink }}</p>
                         <div class="book-details">
-                            <!-- {{ book.title }} -->
+                            <ul>
+                                <li>Title: {{ this.book.title }}</li>
+                                <li>ISBN: {{ this.book.isbn }}</li>
+                                <li>Date: {{ this.book.date }}</li>
+                                <li>Pages: {{ this.book.pages }}</li>
+                                <li>Description: {{ this.book.description }}</li>
+                                <li>Rating: {{ this.book.rating }}</li>
+                                <li>Genre: {{ this.genre.name }}</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -17,23 +29,36 @@
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
     name: "BookView",
     data: () => ({
         book: {},
+        photoLink: "",
+        isReady: false,
+        genre: {},
     }),
-    mounted: function () {
+    mounted:async function () {
         axios
             .get(`http://localhost:9090/books/${this.$route.params.id}`)
             .then((res) => {
-                console.log(res.data);
+                // console.log(res.data);
+            // this.book = JSON.parse(res.data);
             this.book = res.data;
-            console.log(this.book);
+            this.genre = this.book.genre;
+            this.photoLink = '../assets/' + this.book.photo;
+            // console.log(this.photoLink);
+            // console.log(this.book);
+            // console.log(this.genre.name);
+            // this.isReady = true;
         })
             .catch((error) => {
             console.log(error);
         });
+
+        // axios
+        //     .get(`http:localhost:9090/genres/${this.book.genre}`)
     },
 };
 </script>
